2a254194b788bea2a5b5add9f31ac783e779541c
t105590017 <t105590017@ntut.org.tw>
2018-12-04 15:15:54 +0800

    DataBaseWeb_V1

---
+++ DataBaseWeb/assets/js/Test.js
@@ -0,0 +1,12 @@
+/*
+index.html id
+*/
+//index圖片註解
+var nv = document.getElementById("SliderImgDescription_1");
+nv.innerHTML="ID = SliderImgDescription_1";
+var nv = document.getElementById("SliderImgDescription_2");
+nv.innerHTML="ID = SliderImgDescription_2";
+var nv = document.getElementById("SliderImgDescription_3");
+nv.innerHTML="ID = SliderImgDescription_3";
+var nv = document.getElementById("SliderImgDescription_4");
+nv.innerHTML="ID = SliderImgDescription_4";
